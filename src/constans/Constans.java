dd5b2d4462990cc808a047af1518cfca06175af7
«dima» <dima-titov-93@mail.ru>
2023-09-01 22:23:26 +0300

    Использованы константы. Для себя пока не нашёл их преминения, кроме математических задач. Буду больше практиковаться

---
+++ src/constans/Constans.java
@@ -0,0 +1,7 @@
+package constans;
+
+public interface Constans {
+  double DIGIT_PI = 3.14;
+  int meters = 1000;
+
+}
---
+++ src/constans/Main.java
@@ -0,0 +1,8 @@
+package constans;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Число пи равно: " + Constans.DIGIT_PI);
+        System.out.println("В одном километре = " + Constans.meters + " " + "метров");
+    }
+}
